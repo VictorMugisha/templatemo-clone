e3cdd1559b2fd9f0d95c8aeb95be37e49ffd2aac
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-05-07 16:09:33 +0200

    Implemented Banner and Features Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                customWhite: "rgba(255, 255, 255, 0.10)", // Light white color with opacity 50%
+                customWhite: "rgba(255, 255, 255, 0.10)", // Light white color with an opacity of 90%
+                customBlack: "rgba(0, 0, 0, 0.25)", // Transparent black color with an opacity of 50%
             },
         },
     },
